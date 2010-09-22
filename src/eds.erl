34a2026e83137a20caf6006960ec4f99c7bc08cd
wizzard <wizzard@5dce72e8-c536-11df-a5c9-5d66b11d09e4>
2010-09-22 08:37:48 +0000

    git-svn-id: http://synrc.com/repository/eds@2 5dce72e8-c536-11df-a5c9-5d66b11d09e4

--- src/eds.erl
+++ src/eds.erl
@@ -34,24 +34,17 @@ message(No,Message,Socket) ->
 	io:format("messageID: ~p~n",[No]),
 	io:format("~p~n",[Message]),
 	case Message of
-
-		{bindRequest, {'BindRequest',Type,Uid,Auth}} -> 
-
-									bind(No,Uid,Auth,Socket);
-   
+		{bindRequest, {'BindRequest',Type,Uid,Auth}} -> bind(No,Uid,Auth,Socket);
+		{abandonRequest,Type} -> abandon(No,Socket);
+		{unbindRequest, Null} -> abandon(0,Socket);
 		{searchRequest,	{'SearchRequest',SearchDN,Scope,Deref,SizeLimit,
-			TimeLimit,TypesOnly,Filter,Attributes}} -> 
-
-									search(No, SearchDN, Scope,Deref,SizeLimit,
-										TimeLimit,TypesOnly, Filter,Attributes,Socket);
-
-		{abandonRequest,Type} -> 
-									abandon(No,Socket);
-
-		_Else -> 
-			{Msg, Body} = Message,
-			unknown(Msg,Socket)
+			TimeLimit,TypesOnly,Filter,Attributes}} -> search(No, SearchDN, Scope,Deref,SizeLimit,
+			TimeLimit,TypesOnly, Filter,Attributes,Socket)
 	end.
+
+bind(No,Uid,Auth,Socket) ->
+	Response = #'BindResponse'{resultCode = success, matchedDN = Uid, diagnosticMessage = "OK"},
+	answer(Response,No,bindResponse,Socket).
 
 answer(Response,No,ProtocolOp,Socket) ->
 	Message = #'LDAPMessage'{messageID = No, protocolOp = {ProtocolOp, Response}},
@@ -59,25 +52,23 @@ answer(Response,No,ProtocolOp,Socket) ->
 	io:format("~p~n", [Message]),
 	gen_tcp:send(Socket, list_to_binary(Bytes)).
 
-bind(No,Uid,Auth,Socket) ->
-	Response = #'BindResponse'{resultCode = success, matchedDN = Uid, diagnosticMessage = "OK"},
-	answer(Response,No,bindResponse,Socket).
-
 search(No,SearchDN,Scope,Deref,SizeLimit,TimeLimit,TypesOnly,Filter,Attributes,Socket) ->
 	CN = #'PartialAttribute'{type = "cn", vals = ["Oleg Smirnov"]},
-	Email = #'PartialAttribute'{type = "email", vals = [value = "oleg.smirnov@gmail.com"]},
+	Email = #'PartialAttribute'{type = "mail", vals = ["oleg.smirnov@gmail.com"]},
 	Response = #'SearchResultEntry'{
 		objectName = "cn=Oleg Smirnov,ou=Contacts,uid=mes,ou=People,dc=eba,dc=li",
 		attributes = [CN,Email]
 	},
 	answer(Response,No,searchResEntry,Socket),
+	CN2 = #'PartialAttribute'{type = "cn", vals = ["Maxim Sokhatsky"]},
+	Email2 = #'PartialAttribute'{type = "mail", vals = ["maxim.sokhatsky@gmail.com"]},
+	Response2 = #'SearchResultEntry'{
+		objectName = "cn=Maxim Sokhatsky,ou=Contacts,uid=mes,ou=People,dc=eba,dc=li",
+		attributes = [CN2,Email2]
+	},
+	answer(Response2,No,searchResEntry,Socket),
 	Done = #'LDAPResult'{resultCode = success, matchedDN = SearchDN, diagnosticMessage = "OK"},
 	answer(Done,No,searchResDone,Socket).
 
 abandon(No,Socket) ->
 	gen_tcp:close(Socket).
-
-unknown(Message,Socket) ->
-	case Message of
-		{Msg, Data} -> io:format("Unknown message ~p~n", [Msg])
-	end.
